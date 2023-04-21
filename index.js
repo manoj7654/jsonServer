0af866f3e76d06ae7f1312e8d3405c5739232162
Manoj Kumar <manojsfstm5@gmail.com>
2023-04-21 13:50:31 +0530

    key added

--- index.js
+++ index.js
@@ -16,3 +16,15 @@ const PORT = 8000
 server.listen(PORT, () => {
   console.log(`JSON Server is running on http://localhost:${PORT}`)
 })
+// const jsonServer = require('json-server')
+
+// const server = jsonServer.create()
+
+// const router = jsonServer.router('db.json')
+// const middlewares = jsonServer.defaults()
+ 
+// server.use(middlewares)
+// server.use('/api', router)
+// server.listen(process.env.PORT || 5000, () => {
+//   console.log('JSON Server is running')
+// })
